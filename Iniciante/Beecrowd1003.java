a33a0d6853eb724f05fc5f28c06d29e8bfaabe1e
Guilherme Grego <93603791+GregoSX@users.noreply.github.com>
2022-07-11 20:43:24 -0300

    Soma Simples

--- Iniciante/Beecrowd1003.java
+++ Iniciante/Beecrowd1003.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.*;
- 
+
 public class Beecrowd1003 {
     public static void main(String[] args) throws IOException {
         Scanner entrada = new Scanner(System.in);
