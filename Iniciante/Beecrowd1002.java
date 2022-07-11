63412ee472fd25d0d1a12cf3a5b4ee10f1fc6fa8
Guilherme Grego <93603791+GregoSX@users.noreply.github.com>
2022-07-11 20:43:45 -0300

    Área do Círculo

--- Iniciante/Beecrowd1002.java
+++ Iniciante/Beecrowd1002.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.util.*;
- 
+
 public class Beecrowd1002 {
     public static void main(String[] args) throws IOException {
         Scanner entrada = new Scanner(System.in);
