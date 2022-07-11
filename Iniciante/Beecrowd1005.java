ab55380f8cde5a790a53fd6afb93481fa704cda4
Guilherme Grego <93603791+GregoSX@users.noreply.github.com>
2022-07-11 20:42:45 -0300

    Média 1

--- Iniciante/Beecrowd1005.java
+++ Iniciante/Beecrowd1005.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.*;
-
 public class Beecrowd1005 {
     public static void main(String[] args) throws IOException {
         Scanner entrada = new Scanner(System.in);
